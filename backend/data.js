cd1f1ebf596c13c72e3af3043d4a13b03b4b7956
zygoh1986 <69612872+zygoh1986@users.noreply.github.com>
2021-02-04 15:27:35 +0800

    adding backend

---
+++ backend/data.js
@@ -0,0 +1,51 @@
+const data = {
+
+    products:[
+        {
+            _id: '1',
+            year: 1,
+            name: 'Public Housing',
+            countInStock: 2,
+            priceValue: 300000,
+            fPrice: 76800,
+            aPrice: 16800,
+            image: '../images/BHAP01.png',
+            description: 'High floor, 100 m2 5-room public housing @Ephesus 20% down payment, first year only: $60,000. 20 years loan at 1.5% interest per annum: $15,600 Annual insurance and maintenance, $1,200 Eligibility: 1st time married citizens only',
+        },
+        {
+            _id: '2',
+            year: 1,
+            name: 'Apartment',
+            countInStock: 1,
+            priceValue: 430000,
+            fPrice: 110160,
+            aPrice: 24160,
+            image: '../images/BHAP02.png',
+            description: '100 m2, 3-bedroom apartment @Kidron Valley 20% down payment, first year only: $86,000 20 years loan at 1.5% interest per annum: $22,360 Annual insurance and maintenance, $1,800 Facilities: Car park Eligibility: Citizens and PRs only',
+        },
+        {
+            _id: '3',
+            year: 1,
+            name: 'Condo',
+            countInStock: 1,
+            priceValue: 650000,
+            fPrice: 202600,
+            aPrice: 42600,
+            image: '../images/BHAP03.png',
+            description: '100 m2, 3-bedroom condominium @Mount Gerizim 20% down payment, first year only: $130,000 20 years loan at 1.5% interest per annum: $33,800 Annual insurance and maintenance, $3,000 Facilities: Swimming pool, tennis court, BBQ pit, gym, function room, security Eligibility: No restrictions',
+        },
+        {
+            _id: '4',
+            year: 1,
+            name: 'Landed',
+            countInStock: 1,
+            priceValue: 800000,
+            fPrice: 202600,
+            aPrice: 42600,
+            image: '../images/BHAP04.png',
+            description: '300 m2, 5-bedroom, terrace house @Decapolis 20% down payment, first year only: $160,000 20 years loan at 1.5% interest per annum: $41,600 Annual insurance and maintenance, $1,000 Eligibility: Citizens and PRs only',
+        },
+    ],
+};
+
+export default data;
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,17 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Product from '../components/Product';
+import axios from 'axios';
 import data from '../data';
+import LoadingBox from '../components/LoadingBox';
+import MessageBox from '../components/MessageBox';
 
 export default function HomeScreen() {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  useEffect(() => {
+    const fecthData = async () => {
+      try {
+        setLoading(true);
+        const { data } = await axios.get('/api/products');
+        setLoading(false);
+        setProducts(data);
+      } catch (err) {
+        setError(err.message);
+        setLoading(false);
+      }
+    };
+    fecthData();
+  }, []);
   return (
     <div>
-      <div className="row center">
-        {data.products.map((product) => (
-            <a href={`/product/${product._id}`}>
-          <Product key={product._id} product={product}></Product>
-          </a>
-        ))}
-      </div>
+      {loading ? (
+        <LoadingBox></LoadingBox>
+      ) : error ? (
+        <MessageBox variant="danger">{error}</MessageBox>
+      ) : (
+            <div className="row center">
+              {products.map((product) => (
+                <a href={`/product/${product._id}`}>
+                  <Product key={product._id} product={product}></Product>
+                </a>
+              ))}
+
+            </div>
+          )}
     </div>
   );
 }
+
